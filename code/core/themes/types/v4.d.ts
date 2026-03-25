4c6b9d0cd927b270205eed357767f8c56d77aebd
Hanzo Dev <dev@hanzo.ai>
2026-03-24 21:32:30 -0700

    fix: use workspace:* for @hanzo/gui internal deps, fix install

--- code/core/themes/types/v4.d.ts
+++
@@ -1,5 +0,0 @@
-export { tokens } from './v4-tokens';
-export { createThemes } from '@hanzogui/theme-builder';
-export { themes as guiThemes } from './generated-v4-gui';
-export { themes as defaultThemes } from './generated-v4';
-//# sourceMappingURL=v4.d.ts.map

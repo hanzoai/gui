03084da0ee490b515ee8260218865d26cffb40f3
Zach Kelling <z@zeekay.io>
2026-03-24 21:32:30 -0700

    fix: use workspace:* for @hanzo/gui internal deps, fix install

--- code/core/themes/types/index.d.ts
+++
@@ -1,9 +0,0 @@
-export * from './themes';
-export * from './tokens';
-export * from './componentThemeDefinitions';
-export * from './palettes';
-export * from '@hanzogui/colors/legacy';
-export * from './templates';
-export * from './shadows';
-export * from '@hanzogui/theme-builder';
-//# sourceMappingURL=index.d.ts.map

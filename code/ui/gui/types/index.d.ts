28f4f49a3e7d3da42e0dce39d33988ea75ba3a99
Hanzo Dev <dev@hanzo.ai>
2026-03-27 10:44:45 -0700

    feat: @hanzo/gui@2.0.8 - core + stacks, working dist

--- code/ui/gui/types/index.d.ts
+++ code/ui/gui/types/index.d.ts
@@ -1,25 +1,4 @@
-export * from '@hanzogui/stacks';
 import './setup';
-export * from '@hanzogui/compose-refs';
-export * from '@hanzogui/create-context';
-export * from '@hanzogui/font-size';
-export * from '@hanzogui/react-native-media-driver';
-export * from '@hanzogui/helpers';
-export * from '@hanzogui/theme';
-export * from '@hanzogui/use-controllable-state';
-export * from '@hanzogui/use-debounce';
-export * from '@hanzogui/use-force-update';
-export * from '@hanzogui/element';
-export * from '@hanzogui/use-window-dimensions';
-export * from '@hanzogui/dialog';
-export * from '@hanzogui/adapt';
-export * from '@hanzogui/sheet';
-export * from '@hanzogui/visually-hidden';
-export * from './createGui';
-export * from './viewTypes';
-export * from './views/GuiProvider';
-export * from './views/EnsureFlexed';
-export * from './views/Text';
-export type { TransitionKeys, TransitionProp, ColorTokens, CreateGuiConfig, CreateGuiProps, FontColorTokens, FontLanguages, FontLetterSpacingTokens, FontLineHeightTokens, FontFamilyTokens, FontSizeTokens, FontStyleTokens, FontTokens, FontTransformTokens, FontWeightTokens, GenericFont, GenericStackVariants, GenericGuiConfig, GenericTextVariants, GetAnimationKeys, GetProps, GetRef, GetThemeValueForKey, GroupNames, Longhands, Media, MediaPropKeys, MediaQueries, MediaQueryState, RadiusTokens, Shorthands, SizeTokens, SpaceTokens, SpecificTokens, StackNonStyleProps, ViewProps, StaticConfig, Styleable, GuiBaseTheme, GuiBuildOptions, GuiComponent, GuiConfig, GuiCustomConfig, GuiElement, GuiInternalConfig, GuiProviderProps, GuiSettings, GuiTextElement, TextNonStyleProps, TextProps, ThemeKeys, ThemeName, ThemeParsed, ThemeProps, Themes, ThemeTokens, ThemeValueFallback, Token, Tokens, TypeOverride, Variable, VariantSpreadExtras, VariantSpreadFunction, ZIndexTokens, ViewStyle, TextStyle, } from '@hanzogui/core';
-export { ClientOnly, Configuration, ComponentContext, GroupContext, FontLanguage, Theme, View, createComponent, createFont, createShorthands, createStyledContext, createTokens, createVariable, getConfig, getMedia, getCSSStylesAtomic, getThemes, getToken, getTokenValue, getTokens, getVariable, getVariableName, getVariableValue, insertFont, setConfig, setupDev, _withStableStyle, isBrowser, isChrome, isClient, isServer, isGuiComponent, isGuiElement, isTouchable, isVariable, isWeb, isWebTouchable, matchMedia, mediaObjectToString, mediaQueryConfig, mediaState, setOnLayoutStrategy, styled, themeable, useClientValue, useDidFinishSSR, useEvent, useGet, useIsTouchDevice, useIsomorphicLayoutEffect, useMedia, useProps, usePropsAndStyle, useStyle, useConfiguration, useTheme, useThemeName, variableToString, withStaticProperties, } from '@hanzogui/core';
+export * from '@hanzogui/core';
+export * from '@hanzogui/stacks';
 //# sourceMappingURL=index.d.ts.map

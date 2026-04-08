95d9562e1b605882554bd129f47e6164ab4a69fb
Hanzo AI <dev@hanzo.ai>
2026-04-07 18:55:22 -0700

    chore: regenerate types after upstream sync build

--- code/packages/sandbox-ui/types/TestSolito.d.ts
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import type { LinkProps } from 'solito/link';
-import type { AnchorProps } from '@hanzo/gui';
-export type TextLinkProps = Pick<LinkProps, 'href' | 'target'> & AnchorProps;
-export declare const TextLink: React.ForwardRefExoticComponent<any>;
-//# sourceMappingURL=TestSolito.d.ts.map
--- code/packages/sandbox-ui/types/index.d.ts
+++
@@ -1,8 +0,0 @@
-export * from '@hanzo/gui';
-export * from '@hanzogui/toast';
-export * from './SandboxHeading';
-export * from './views';
-export declare const SimpleTest: import("@hanzo/gui").GuiComponent<import("@hanzogui/web").TamaDefer, import("@hanzo/gui").GuiElement, import("@hanzogui/core").RNGuiViewNonStyleProps, import("@hanzogui/web").StackStyleBase, {}, import("@hanzogui/web").StaticConfigPublic>;
-export declare const Test14Component: import("@hanzo/gui").GuiComponent<import("@hanzogui/web").TamaDefer, any, any, any, any, any>;
-export declare const TestBorderExtraction: import("@hanzo/gui").GuiComponent<import("@hanzogui/web").TamaDefer, any, any, any, any, any>;
-//# sourceMappingURL=index.d.ts.map

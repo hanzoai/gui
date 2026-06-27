a8d3bca5e1a8a2dcc63dd90679e21324710262bd
hanzo-dev <dev@hanzo.ai>
2026-06-27 03:35:15 +0000

    build(ai): rebuild dist (wallet auto-init + zen icon)

---
+++ pkgs/ai/dist-desktop/mermaid-GHXKKRXX-DkRsC-Re.cjs
@@ -0,0 +1,9 @@
+'use strict';
+
+Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+
+const index = require('./index-DKX_wW-G.cjs');
+
+
+
+exports.Mermaid = index.po;
---
+++ pkgs/ai/dist-desktop/mermaid-GHXKKRXX-rNxjh_2f.js
@@ -0,0 +1,5 @@
+import { p as po } from './index-CjXUZi2M.js';
+
+
+
+export { po as Mermaid };

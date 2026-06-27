b9bbf521a840a65cff12532e154be66617c7851c
Zach Kelling <z@zeekay.io>
2026-06-27 03:35:15 +0000

    build(ai): rebuild dist (wallet auto-init + zen icon)

---
+++ pkgs/ai/dist-desktop/highlighted-body-OFNGDK62-DVKk8YxY.cjs
@@ -0,0 +1,11 @@
+'use strict';
+
+Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+
+const index = require('./index-DKX_wW-G.cjs');
+const React = require('react');
+const jsxRuntime = require('react/jsx-runtime');
+
+var R=({code:s,language:e,raw:t,className:h,startLine:d,lineNumbers:m,...p})=>{let{shikiTheme:l}=React.useContext(index.R),o=index.Li(),[a,i]=React.useState(t);return React.useEffect(()=>{if(!o){i(t);return}let r=o.highlight({code:s,language:e,themes:l},c=>{i(c);});r&&i(r);},[s,e,l,o,t]),jsxRuntime.jsx(index.At,{className:h,language:e,lineNumbers:m,result:a,startLine:d,...p})};
+
+exports.HighlightedCodeBlockBody = R;
--- pkgs/ai/dist-desktop/mermaid-GHXKKRXX-C6oWc3e-.cjs
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-
-const index = require('./index-BqtStGzd.cjs');
-
-
-
-exports.Mermaid = index.po;
